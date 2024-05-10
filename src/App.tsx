cc396d9b45da93b3ed88dd8a0967cd9901335b02
mark-miller <mark@thirdcoastfederal.com>
2024-05-10 16:00:26 -0400

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { googleLogout } from "@react-oauth/google";
+import { useState } from "react";
+import Login from "./Login";
 
 function App() {
+  const [user, setUser] = useState<any>();
+  const [profile, setProfile] = useState<any>([]);
+
+  // log out function to log the user out of google and set the profile array to null
+  const logOut = () => {
+    googleLogout();
+    setProfile(null);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>React Google Login</h2>
+      <br />
+      <br />
+      {profile ? (
+        <div>
+          <img src={profile.picture} alt="user image" />
+          <h3>User Logged in</h3>
+          <p>Name: {profile.name}</p>
+          <p>Email Address: {profile.email}</p>
+          <br />
+          <br />
+          {JSON.stringify(profile)}
+          <button onClick={logOut}>Log out</button>
+        </div>
+      ) : (
+        <div className="shadow-2xl">
+          <Login setProfile={setProfile} />
+        </div>
+      )}
     </div>
   );
 }
-
 export default App;
---
+++ src/Login.tsx
@@ -0,0 +1,48 @@
+import { useGoogleLogin } from "@react-oauth/google";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+const Login = ({ setProfile }: { setProfile: any }) => {
+  const [user, setUser] = useState<any>([]);
+
+  const login = useGoogleLogin({
+    onSuccess: (codeResponse) => {
+      setUser(codeResponse);
+      console.log(codeResponse);
+    },
+    onError: (error) => console.log("Login Failed:", error),
+  });
+
+  useEffect(() => {
+    if (user) {
+      axios
+        .get(
+          `https://www.googleapis.com/oauth2/v1/userinfo?access_token=${user.access_token}`,
+          {
+            headers: {
+              Authorization: `Bearer ${user.access_token}`,
+              Accept: "application/json",
+            },
+          }
+        )
+        .then((res) => {
+          setProfile(res.data);
+        })
+        .catch((err) => console.log(err));
+    }
+  }, [user]);
+
+  return (
+    <div className="shadow-2xl">
+      <button
+        type="button"
+        className="bg-mainColor flex justify-center items-center p-3 rounded-lg cursor-pointer outline-none"
+        onClick={() => login()}
+      >
+        Sign in with Google
+      </button>
+    </div>
+  );
+};
+
+export default Login;
