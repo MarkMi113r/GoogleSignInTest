ab7ad959c16445c34d5858b7ce371aae757b38ed
mark-miller <mark@thirdcoastfederal.com>
2024-06-25 12:24:30 -0400

    construction under way

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,20 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import { GoogleOAuthProvider } from "@react-oauth/google";
+import "./index.css";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  <GoogleOAuthProvider clientId="6820783371-5i5301c4dcmcrhpq6pv08dbh5497qp2i.apps.googleusercontent.com">
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </GoogleOAuthProvider>
+  // <GoogleOAuthProvider clientId="6820783371-5i5301c4dcmcrhpq6pv08dbh5497qp2i.apps.googleusercontent.com">
+  //   <React.StrictMode>
+  //     <App />
+  //   </React.StrictMode>
+  // </GoogleOAuthProvider>
+  <App />
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//reportWebVitals();
